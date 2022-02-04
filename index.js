3a30c04a9892a23ce36f74f8c3a5bd8b3bf40c09
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-02-04 22:05:39 +0100

    more crazy stuff

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+const audio = document.createElement("audio")
+audio.src="https://orangefreesounds.com/wp-content/uploads/2021/11/Cat-meowing-loudly-sound.mp3"
+
+
+audio.play()
+// setTimeout(audio.play, 3000)
+
+
+
+
+
+

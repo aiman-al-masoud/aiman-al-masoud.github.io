d897b63aacd9ceb2d0921e2b8eef58f0ee4ab18b
Aiman Al Masoud <80338230+aiman-al-masoud@users.noreply.github.com>
2022-02-05 15:13:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 
-const audio = document.createElement("audio")
-audio.src="https://orangefreesounds.com/wp-content/uploads/2021/11/Cat-meowing-loudly-sound.mp3"
+// const audio = document.createElement("audio")
+// audio.src="https://orangefreesounds.com/wp-content/uploads/2021/11/Cat-meowing-loudly-sound.mp3"
 
 
-audio.play()
-// setTimeout(audio.play, 3000)
+// audio.play()
+// // setTimeout(audio.play, 3000)
 
 
 

e1341a27079e2e5ae18c32901e7731220b055ce1
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-03-16 19:02:02 +0100

    testing js speech recognition api

---
+++ javascript_speech/mwe.js
@@ -0,0 +1,40 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var colors = ['aqua', 'azure', 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+
+recognition.onresult = function (event) {
+    var color = event.results[0][0].transcript;
+    console.log(color)
+}
+
+
+recognition.onspeechend = function () {
+    recognition.stop();
+}
+
+recognition.onnomatch = function (event) {
+    console.log(event)
+}
+
+recognition.onerror = function (event) {
+    console.log(event)
+}
+
+document.getElementById("start").addEventListener("click", ()=>{
+    recognition.start()
+})
+
+
